e39e2e0282ac85568b65da5aed57a1177bd8727b
Ildar Shamiev <shamief@yandex.ru>
2024-04-01 16:13:04 +0300

    ch05 ex13

--- exercises/ch05/13_calendar.js
+++ exercises/ch05/13_calendar.js
@@ -1,26 +1,27 @@
 function calendar(month, year, locale = 'ru-RU') {
-    let date = new Date(year, month - 1);
+    let date = new Date(Date.UTC(year, month - 1));
     let monthStartDay = locale == 'ru-RU' ? date.getDay() - 1: date.getDay();
     let lastDate = lastMonthDate(date);
     let days = monthDays(monthStartDay, lastDate);
     let startWeek = 0;
     let endWeek = 7;
-    let calendar = [];
+    let cal = [date.toLocaleDateString(locale, {month: 'long', year: 'numeric'})];
 
     while (startWeek < lastDate + monthStartDay) {
-        calendar.push(days.slice(startWeek, endWeek).join(' '));
+        cal.push(days.slice(startWeek, endWeek).join(' '));
         startWeek = endWeek;
         endWeek += 7;
     }
-    return calendar.join('\n');
+    return cal.join('\n');
 }
 
 function lastMonthDate(date) {
-    let current = date.getMonth();
+    let currentDate = new Date(date);
+    let month = currentDate.getMonth();
     let lastDate = 28;
-    while (current === date.getMonth()) {
+    while (month === currentDate.getMonth()) {
         lastDate++;
-        date.setDate(lastDate);
+        currentDate.setDate(lastDate);
     }
     return lastDate - 1;
 }
