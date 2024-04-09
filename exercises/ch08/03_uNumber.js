7c7e992dffce3cb66342afc2ae4629b1e0159188
Ildar Shamiev <shamief@yandex.ru>
2024-04-09 11:04:05 +0300

    ch08 ex03

---
+++ exercises/ch08/03_uNumber.js
@@ -0,0 +1,6 @@
+console.log((123).toLocaleString('en-u-nu-latn'));
+console.log((123).toLocaleString('ar-u-nu-arab'));
+console.log((123).toLocaleString('hi-u-nu-deva'));
+console.log((123).toLocaleString('hi-u-nu-tamldec'));
+console.log((123).toLocaleString('th-u-nu-thai'));
+console.log((123).toLocaleString('zh-u-nu-hanidec'));
