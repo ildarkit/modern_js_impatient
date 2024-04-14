dbc247b9e62bad0b524e5b5932485d00909fbdac
Ildar Shamiev <shamief@yandex.ru>
2024-04-14 15:41:55 +0300

    ch09 ex09

---
+++ exercises/ch09/09_loadImageAwait.js
@@ -0,0 +1,36 @@
+const loadImage = url => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', url);
+        request.responseType = 'blob';
+
+        const getImage = () => {
+            if (request.status == 200) { 
+                const blob = new Blob([request.response], {type: 'image/png'});
+                const img = document.createElement('img');
+                img.src = URL.createObjectURL(blob);
+                resolve(img);
+            } else
+                reject(Error(`${request.status}: ${request.statusText}`));
+        };
+        request.addEventListener('load', getImage);
+        request.addEventListener('error', () => reject(Error('Network error')));
+        request.send();
+    });
+};
+
+let urls = [
+    '/bookcode/hanafuda/1-1.png',
+    '/bookcode/hanafuda/1-2.png',
+    '/bookcode/hanafuda/1-3.png',
+    '/bookcode/hanafuda/1-4.png',
+    'https://commons.wikimedia.org/wiki/File:17_layer_cube.png#/media/%D0%A4%D0%B0%D0%B9%D0%BB:17_layer_cube.png'
+];
+const promises = urls.map(loadImage);
+const results = await Promise.allSettled(promises);
+const imgdiv = document.getElementById('images');
+
+for (const result of results)
+    if (result.status === 'fulfilled')
+        imgdiv.appendChild(result.value);
+    else console.error(result.reason);
