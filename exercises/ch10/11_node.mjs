71881130acfd4bf908c6ee21fe7fd22a6e98315c
Ildar Shamiev <shamief@yandex.ru>
2024-04-17 14:42:36 +0300

    ch10 ex11 parent children

--- exercises/ch10/11_node.mjs
+++ exercises/ch10/11_node.mjs
@@ -4,6 +4,6 @@ import Parent from './11_parent.mjs'
 export default class Node {
     static from(value, ...children) {
         return children.length === 0 ? new Leaf(value)
-            : new Parent(value);
+            : new Parent(value, children);
     }
 }
