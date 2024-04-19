05824bd5323fec80f5503cc37a391a606ce30b00
Ildar Shamiev <shamief@yandex.ru>
2024-04-19 14:12:27 +0300

    ch11 ex05

---
+++ exercises/ch11/05_descriptors.js
@@ -0,0 +1,3 @@
+console.log(Object.getOwnPropertyDescriptors([1, 2, 3]));
+console.log(Object.getOwnPropertyDescriptors([1, 2, 3].constructor));
+console.log(Object.getOwnPropertyDescriptors([1, 2, 3].prototype));
