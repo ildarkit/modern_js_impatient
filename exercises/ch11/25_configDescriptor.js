b8d4f558a027a3d7fa85e09c77e08a3f9a486be8
Ildar Shamiev <shamief@yandex.ru>
2024-04-23 13:42:03 +0300

    ch11 ex25

---
+++ exercises/ch11/25_configDescriptor.js
@@ -0,0 +1,34 @@
+const createRange = (start, end) => {
+    const isIndex = key =>
+        typeof key === 'string' && /^[0-9]+$/.test(key) && parseInt(key) < end - start;
+    return new Proxy({}, {
+        get: (target, key, receiver) => {
+            if (isIndex(key)) {
+                return start + parseInt(key);
+            } else {
+                return Reflect.get(target, key, receiver);
+            }
+        },
+        ownKeys: target => {
+            const result = Reflect.ownKeys(target);
+            for (let i = 0; i < end - start; i++)
+                result.push(String(i));
+            return result;
+        },
+        getOwnPropertyDescriptor: (target, key) => {
+            if (isIndex(key)) {
+                return {
+                    value: start + Number(key),
+                    writable: false,
+                    enumerable: true,
+                    configurable: true
+                };
+            } else {
+                return Reflect.getOwnPropertyDescriptor(target, key);
+            }
+        }
+    });
+};
+
+const range = createRange(10, 100);
+console.log(Object.keys(range));
